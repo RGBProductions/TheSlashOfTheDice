761c5e8b4cb3b682c358b6d1486f1651c1e79cde
RGB Productions <luvm1n3craft@gmail.com>
2024-07-07 12:25:43 -0700

    Calm mode not unlocked popup

--- default/menus/play.lua
+++ default/menus/play.lua
@@ -124,6 +124,54 @@ local playMenu = UI.Element:new({
                         if Achievements.IsUnlocked("default_30_waves") then
                             SceneManager.LoadScene("scenes/game", {mode = self.id})
                         else
+                            local popup = UI.Panel:new({
+                                width = 480,
+                                height = 320,
+                                background = function() return GetTheme().popup_error.background end,
+                                border = function() return GetTheme().popup_error.border end,
+                                children = {
+                                    UI.Text:new({
+                                        clickThrough = true,
+                                        x = 0,
+                                        y = -40,
+                                        width = 480,
+                                        height = 208,
+                                        text = function() return Localize("calm_not_unlocked") end,
+                                        font = lgfont_2x,
+                                        fontScale = 0.5,
+                                        alignHoriz = "center",
+                                        alignVert = "center"
+                                    }),
+                                    UI.Button:new({
+                                        id = "close",
+                                        width = 256,
+                                        height = 64,
+                                        y = 112,
+                                        background = function() return GetTheme().button_secondary.background end,
+                                        border = function() return GetTheme().button_secondary.border end,
+                                        onclick = function(me)
+                                            table.remove(Dialogs, table.index(Dialogs, me.parent))
+                                        end,
+                                        cursor = "hand",
+                                        children = {
+                                            UI.Text:new({
+                                                clickThrough = true,
+                                                x = 0,
+                                                y = 0,
+                                                width = 256,
+                                                height = 64,
+                                                text = function() return Localize("button.ok") end,
+                                                color = function() return GetTheme().button_secondary.text end,
+                                                font = lgfont_2x,
+                                                fontScale = 0.5,
+                                                alignHoriz = "center",
+                                                alignVert = "center"
+                                            })
+                                        },
+                                    })
+                                }
+                            })
+                            table.insert(Dialogs, popup)
                         end
                     end
                 })
